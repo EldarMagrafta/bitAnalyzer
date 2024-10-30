4136221b24837bf7472bd1a6cd8ad01af177bcbe
eldar.magrafta <magraftaeldar@gmail.com>
2024-10-30 20:34:08 +0200

    Add images to instructions

--- src/components/InstructionsOverlay.js
+++ src/components/InstructionsOverlay.js
@@ -1,9 +1,9 @@
 // src/components/InstructionsOverlay.js
-import React from 'react';
-import '../assets/styles/App.css'; // Make sure to import your styles
-import Step1Image from '../assets/images/step1.png';
-import Step2Image from '../assets/images/step2.png';
-import Step3Image from '../assets/images/step3.png';
+import React from "react";
+import "../assets/styles/App.css"; // Make sure to import your styles
+import Step1Image from "../assets/images/app_screenshot1.png";
+import Step2Image from "../assets/images/app_screenshot2.png";
+import Step3Image from "../assets/images/app_screenshot3.png";
 
 const InstructionsOverlay = ({ onClose }) => {
   const handleOverlayClick = () => {
@@ -12,28 +12,46 @@ const InstructionsOverlay = ({ onClose }) => {
 
   return (
     <div className="instructions-overlay" onClick={handleOverlayClick}>
-      <div
-        className="instructions-frame"
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className="instructions-frame" onClick={(e) => e.stopPropagation()}>
         {/* Instructions content here */}
-        <h2>Instructions</h2>
+        <h2>?איך משתמשים</h2>
         <p>Welcome to the CSV Analyzer! Follow these steps to get started:</p>
         <ol>
           <li>
             <strong>Choose Your CSV File:</strong>
-            <p>Click on the <strong>"Choose File"</strong> button to select your CSV file.</p>
-            <img src={Step1Image} alt="Choose File Button" className="instruction-image" />
+            <p>
+              Click on the <strong>"Choose File"</strong> button to select your
+              CSV file.
+            </p>
+            <img
+              src={Step1Image}
+              alt="Choose File Button"
+              className="instruction-image"
+            />
           </li>
           <li>
             <strong>Analyze the Data:</strong>
-            <p>Once selected, click on the <strong>"Analyze"</strong> button to process the data.</p>
-            <img src={Step2Image} alt="Analyze Button" className="instruction-image" />
+            <p>
+              Once selected, click on the <strong>"Analyze"</strong> button to
+              process the data.
+            </p>
+            <img
+              src={Step2Image}
+              alt="Analyze Button"
+              className="instruction-image"
+            />
           </li>
           <li>
             <strong>Explore Your Data:</strong>
-            <p>Use the charts and tables below to explore and interact with your data.</p>
-            <img src={Step3Image} alt="Data Visualization" className="instruction-image" />
+            <p>
+              Use the charts and tables below to explore and interact with your
+              data.
+            </p>
+            <img
+              src={Step3Image}
+              alt="Data Visualization"
+              className="instruction-image"
+            />
           </li>
         </ol>
         <p>Click outside this frame to close the instructions.</p>
@@ -43,6 +61,3 @@ const InstructionsOverlay = ({ onClose }) => {
 };
 
 export default InstructionsOverlay;
-
-
-
