668a9ceebd4199df55407231662b79b8765d13da
eldar.magrafta <magraftaeldar@gmail.com>
2024-10-31 15:13:49 +0200

    change pie chart layout

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,7 +1,7 @@
 // src/components/PieChart.js
-import React from 'react';
-import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -17,13 +17,24 @@ const PieChart = ({ data, onSliceClick }) => {
   };
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <div style={{ width: "100%", height: "100%" }}>
       <Pie
         data={data}
         options={{
-          responsive: true, // Make sure it responds to the container size
-          maintainAspectRatio: false, // Allows the chart to stretch to the container size
+          responsive: true,
+          maintainAspectRatio: false,
           onClick: handleClick,
+          plugins: {
+            legend: {
+              position: "right",
+            },
+            tooltip: {
+              enabled: true,
+            },
+            datalabels: {
+              display: false, // Ensure no data labels are displayed on the pie chart
+            },
+          },
         }}
       />
     </div>
