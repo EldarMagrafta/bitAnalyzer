c81f63415c88166cb4b561f4993a93f837df409d
eldar.magrafta <magraftaeldar@gmail.com>
2024-11-15 18:50:26 +0200

    CSS

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,20 +1,19 @@
-// src/components/Transaction.js
-import React from 'react';
-import '../assets/styles/App.css'; // Ensure the correct path to the CSS file
+import React from "react";
+import "../assets/styles/App.css"; // Ensure the correct path to the CSS file
 
-const Transaction = ({transaction, fields}) => {
-    return (
-        <div className="transaction-container">
-            {fields.map((field, index) => (
-                <React.Fragment key={index}>
-                    <span className="transaction-item">{transaction[field]}</span>
-                    {index < fields.length - 1 && (
-                        <span>|</span>
-                    )}
-                </React.Fragment>
-            ))}
-        </div>
-    );
+const Transaction = ({ transaction, fields }) => {
+  return (
+    <div className="transaction-container">
+      {fields.map((field, index) => (
+        <React.Fragment key={index}>
+          <span className="transaction-item">
+            {field === "amount" ? `${transaction[field]}₪` : transaction[field]}
+          </span>
+          {index < fields.length - 1 && <span>|</span>}
+        </React.Fragment>
+      ))}
+    </div>
+  );
 };
 
 export default Transaction;
